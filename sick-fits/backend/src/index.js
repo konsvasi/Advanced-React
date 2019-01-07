6d8291cc6302ff7e8e7d1a0516322c84ad7c5c2d
Kostas V <konsvass@outlook.com>
2019-01-07 09:32:57 +0200

    created db and server function, added scheme and prisma implementation

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,16 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env'});
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+//TODO Use express middleware to handle cookies 
+//TODO Use express to populate current users
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  },
+}, deets => {
+  console.log(`Server is running on http:/localhost:${deets.port}`);
+});
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,19 @@
-const mutations = {};
+const mutations = {
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if logged in
+    const item = await ctx.db.mutation.createItem({
+      data: { ...args }
+    }, info);
+
+    return item;
+  }
+};
 
 module.exports = mutations;
